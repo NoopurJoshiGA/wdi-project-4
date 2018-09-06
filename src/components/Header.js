ad3bd857b03ec0a5ad0bf2c94ffadd3768450b3d
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-06 12:42:53 +0100

    added Header and navigation

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <nav className="navbar has-background-light">
+          <div className="navbar-brand">BOKE</div>
+          <a role="button" className="navbar-burger is-active" aria-label="menu" aria-expanded="false" data-target="navMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+          <div className="navbar-end">
+            <Link className="navbar-item has-background-primary button" to="/users">Discover</Link>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default withRouter(Header);
