1eda52f9e4c06b2c050c9a81fe5be86ebd1fdef8
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-06 14:37:23 +0100

    added log out functionality (remove token)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import Auth from '../lib/Auth';
 
 class Header extends React.Component {
+
+  handleLogout = () => {
+    Auth.removeToken();
+    console.log('user has logged out...');
+    this.props.history.push('/');
+  }
+
   render() {
     return(
       <header>
@@ -14,8 +22,10 @@ class Header extends React.Component {
           </a>
           <div className="navbar-end">
             <Link className="navbar-item has-background-primary button" to="/users">Discover</Link>
-            <Link className="navbar-item" to="/login">Login</Link>
-            <Link className="navbar-item" to="/register">Register</Link>
+            {!Auth.isAuthenticated() && <Link className="navbar-item" to="/login">Login</Link>}
+            {!Auth.isAuthenticated() && <Link className="navbar-item" to="/register">Register</Link>}
+            {Auth.isAuthenticated() &&
+              <a className="navbar-item" onClick={this.handleLogout}>Log out {Auth.currentUsername()}</a>}
           </div>
         </nav>
       </header>
