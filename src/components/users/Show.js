5d53fbb64746ed60443391a282012dbd0bc10af9
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-06 16:36:22 +0100

    add users images to their profile (filtered images in the axios res)

--- src/components/users/Show.js
+++ src/components/users/Show.js
@@ -19,33 +19,66 @@ class UsersShow extends React.Component {
       .then(() => {
         axios.get('/api/images')
           .then(res => {
-            this.setState({ images: res.data, user: userData });
-            console.log('images are', res.data, 'user is', userData);
+            console.log('res is', res);
+            const images = res.data;
+            console.log('imageData is', imageData);
+            const imageData = images.filter(image => image.uploadedBy === userData._id);
+            console.log('imageData is now', imageData);
+            this.setState({ images: imageData, user: userData });
           });
       });
   }
 
+  findImagesByUser = (user) => {
+    this.state.images.map(image => {
+      if(image.uploadedBy.username === user.username) {
+        console.log('found a match...');
+      }
+    });
+  }
+
   render() {
     const user = this.state.user;
+    const images = this.state.images;
+
     console.log('user is', user);
 
     return(
       <section className="section">
+
         {user &&
           <div className="container columns is-multiline has-text-centered">
             <h3 className="title is-3">{user.firstName} {user.lastName}</h3>
             <h4 className="subtitle is-4">{user.type}</h4>
             <img src={user.profilePic} alt={user.firstName}></img>
             <h4 className="subtitle is-4">{user.postcode}</h4>
-            <p>Interested in: </p>{ user.interests.map(interest =>
-              <div className="tag has-background-light">{interest}</div>
+            <p>Interested in: </p>
+            { user.interests.map(interest =>
+              <div key={user._id} className="tag has-background-light">{interest}</div>
             )}
+
+            {/* Description */}
             <p>{user.description}</p>
+
+            {/* Reviews */}
             {user.reviews.map(review =>
-              <div className="card has-background-info">"{review.reviewAddedBy}{review.content})"</div>)}
+              <div key={user._id}
+                className="card has-background-info">
+                {review.reviewAddedBy}{review.content})
+              </div>
+            )}
+
             {/* TODO: add the user who has commented */}
-            {user.ratings.map(rating => <div className="card">Rating: {rating.number} {rating.ratingBy}</div>)}
+
+            {/* Ratings */}
+            {user.ratings.map(rating => <div key={user._id} className="card">Rating: {rating.number} {rating.ratingBy}</div>)}
             {/* TODO: add the user who has provided a rating */}
+
+            {/* Photos / Portfolio */}
+            <h3 className="title is-3">Photos</h3>
+
+            {images.map(image => <img key={image._id} src={image.imageUrl} />
+            )}
 
             <Link className="button is-primary is-rounded is-outlined" to={`/users/${user._id}/edit`}>Edit Profile</Link>
           </div>
