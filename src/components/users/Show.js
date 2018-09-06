1a8dc8e8d746b459a420f40394b4069126049b1b
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-06 17:39:20 +0100

    added review by in user show page

--- src/components/users/Show.js
+++ src/components/users/Show.js
@@ -29,17 +29,10 @@ class UsersShow extends React.Component {
       });
   }
 
-  findImagesByUser = (user) => {
-    this.state.images.map(image => {
-      if(image.uploadedBy.username === user.username) {
-        console.log('found a match...');
-      }
-    });
-  }
-
   render() {
     const user = this.state.user;
     const images = this.state.images;
+    let averageRating;
 
     console.log('user is', user);
 
@@ -48,37 +41,66 @@ class UsersShow extends React.Component {
 
         {user &&
           <div className="container columns is-multiline has-text-centered">
+
             <h3 className="title is-3">{user.firstName} {user.lastName}</h3>
+
             <h4 className="subtitle is-4">{user.type}</h4>
+
             <img src={user.profilePic} alt={user.firstName}></img>
+
+            {/* Postcode */}
             <h4 className="subtitle is-4">{user.postcode}</h4>
-            <p>Interested in: </p>
-            { user.interests.map(interest =>
-              <div key={user._id} className="tag has-background-light">{interest}</div>
-            )}
+
+            {/* Interests */}
+            <div className="section has-background-light">
+              <p>Interested in: </p>
+              { user.interests.map(interest =>
+                <div key={user._id} className="tag has-background-info has-text-white">{interest}</div>
+              )}
+            </div>
 
             {/* Description */}
-            <p>{user.description}</p>
+            <p className="section has-background-black has-text-white">{user.description}</p>
 
             {/* Reviews */}
-            {user.reviews.map(review =>
-              <div key={user._id}
-                className="card has-background-info">
-                {review.reviewAddedBy}{review.content})
-              </div>
-            )}
+            <div className="section columns">
+              {user.reviews.map(review =>
 
-            {/* TODO: add the user who has commented */}
+                <div key={user._id}
+                  className="has-background-white">
+
+                  <div className="column is-2">
+                    <figure className="image is-64x64">
+                      <img className="is-rounded" src={review.addedBy.profilePic} />
+                    </figure>
+                  </div>
+
+                  <div className="column is-3">{review.addedBy.username}</div>
+                  <div className="column is-3">{review.content}</div>
+                </div>
+              )}
+            </div>
 
             {/* Ratings */}
-            {user.ratings.map(rating => <div key={user._id} className="card">Rating: {rating.number} {rating.ratingBy}</div>)}
-            {/* TODO: add the user who has provided a rating */}
+            {/* // TODO: Get the average rating... use reduce? */}
+            <div className="section columns has-background-black has-text-white">
+              {user.ratings.map(rating =>
+                <p key={rating._id}>{rating.number}</p>
+              )}
+            </div>
 
             {/* Photos / Portfolio */}
-            <h3 className="title is-3">Photos</h3>
+            <h3 className="title is-3">Portfolio</h3>
 
-            {images.map(image => <img key={image._id} src={image.imageUrl} />
-            )}
+            <div className="columns has-background-black">
+              { images.map(image =>
+                <Link key={image._id} to={`/images/${image._id}`}>
+                  <div className="column is-6-mobile">
+                    <img key={image._id} src={image.imageUrl} />
+                  </div>
+                </Link>
+              )}
+            </div>
 
             <Link className="button is-primary is-rounded is-outlined" to={`/users/${user._id}/edit`}>Edit Profile</Link>
           </div>
