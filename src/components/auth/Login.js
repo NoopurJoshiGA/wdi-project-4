9a5e4b752b63ff9bca84e06e88fe1d475e2dab2b
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-06 13:20:36 +0100

    added handleChange function in the Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,21 +5,33 @@ class AuthLogin extends React.Component {
   state = {
     // prepopulate for test purposes
     // TODO: remove this before deployment!
-    email: 'noopurjoshi@email.com',
-    password: 'pass'
+    // email: 'noopurjoshi@email.com',
+    // password: 'pass'
+  }
+
+  handleChange = (event) => {
+    const { target: { name, value }} = event;
+    this.setState({ [name]: value });
+    // console.log(this.state.email, this.state.password);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('logging in user..', this.state.email, this.state.password);
   }
 
   render() {
     return (
       <section>
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit}>
 
           <input
             className="input"
             name="email"
             placeholder="Email"
             type="example@email.com"
-            value={this.state.email}>
+            value={this.state.email || ''}
+            onChange={this.handleChange}>
           </input>
 
           <input
@@ -27,7 +39,8 @@ class AuthLogin extends React.Component {
             name="password"
             placeholder="Password"
             type="password"
-            value={this.state.password}>
+            value={this.state.password || ''}
+            onChange={this.handleChange}>
           </input>
 
           <button className="button is-outlined is-primary" type="submit">Login</button>
