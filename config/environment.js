ba8e4cc4064503bf0d101912e7be6504f720a3bb
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-12 09:59:26 +0100

    added port console log

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/boke';
 const secret = process.env.SECRET || 'boke';
 const port = process.env.PORT || 4000;
+console.log('port is', port);
 
 module.exports = {
   dbURI, secret, port
