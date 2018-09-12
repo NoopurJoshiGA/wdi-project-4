968f23c2b22413cd13aa219b988e7c0f6a3e5fab
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-09-12 10:07:42 +0100

    added dburi console log

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/boke';
 const secret = process.env.SECRET || 'boke';
 const port = process.env.PORT || 4000;
-console.log('port is', port);
+console.log('port is, mongodb is', port, dbURI);
 
 module.exports = {
   dbURI, secret, port
